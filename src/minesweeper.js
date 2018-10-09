73fbb44aaca236c9d221e5fdcf8b29005dd1fcab
Daniel Pereira <danielpaulpereira@protonmail.com>
2018-10-09 17:18:00 -0400

    02_Manually_create_a_simulated_game_board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,5 +1,13 @@
 const blankLine = ' | | ';
+const guessLine = '1| | ';
+const bombLine = ' |B| ';
 
+console.log("A blank board:");
 console.log(blankLine);
 console.log(blankLine);
+console.log(blankLine);
+
+console.log("A board with a guess and a bomb:");
+console.log(guessLine);
+console.log(bombLine);
 console.log(blankLine);
